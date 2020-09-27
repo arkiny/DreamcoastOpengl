05fb203f9c7dd8210a1f9abbcb7f388a6241d58b
Heedong Lee <arkinylee@gmail.com>
2020-09-27 19:11:07 +0900

    freeglut branch

--- DreamCoastOpenGL/DreamCoastOpenGL.cpp
+++ DreamCoastOpenGL/DreamCoastOpenGL.cpp
@@ -2,54 +2,65 @@
 //
 
 #include <iostream>
-#include "glfw3.h"
+#include "freeglut.h"
+#include <gl/GLU.h>
 
-int main()
+void InitLight()
+{
+    GLfloat mat_diffuse[] = {.5, .4, .3, 1};
+    GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat mat_ambient[] = {.5, .4, .3, 1};
+    GLfloat mat_shininess[] = { 15 };
+
+    GLfloat light_specular[] = {1,1,1,1};
+    GLfloat light_diffuse[] = {.8, .8, .8 , 1.0};
+    GLfloat light_ambient[] = { .3, .3, .3, 1.0 };
+    GLfloat light_position[] = {-3, 6, 3, 0};
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+void MyDisplay()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0, 0, 0, 0, -1, 0, 1, 0);
+    glutSolidTeapot(0.5);
+    glFlush();
+}
+
+void MyReshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1, 1, -1, 1, -1, 1);
+}
+
+int main(int argc, char** argv)
 {
     std::cout << "Hello World!\n";
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        auto MyDisplay = [](GLFWwindow* window) {
-            glClear(GL_COLOR_BUFFER_BIT);
-            glBegin(GL_POLYGON);
-            {
-                glVertex3f(-0.5f, -0.5f, 0.0f);
-                glVertex3f(0.5f, -0.5f, 0.0f);
-                glVertex3f(0.5f, 0.5f, 0.0f);
-                glVertex3f(-0.5f, 0.5f, 0.0f);
-            }
-            glEnd();
-            glFlush();
-        };
-
-        MyDisplay(window);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("OpeGL Sample Drawing");
+    glClearColor(0.4, 0.4, 0.4, 0.0);
+    InitLight();
+    glutDisplayFunc(MyDisplay);
+    glutReshapeFunc(MyReshape);
+    glutMainLoop();
     return 0;
 }
 
